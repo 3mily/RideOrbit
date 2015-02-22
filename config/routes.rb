7483ee01417a087604696c6e250ddc4925702857
Emily <emrcraig@gmail.com>
2015-02-21 22:09:33 -0800

    redo routes, remove unnecessary namespacing

--- config/routes.rb
+++ config/routes.rb
@@ -4,13 +4,13 @@ Rails.application.routes.draw do
 
   resource :session, only: [:new, :create, :destroy]
 
-  resources :users do
-    resource :places, only: [:create, :destroy, :show]
-    resources :commutes, only: [:create, :new, :destroy, :show] do
-      resource :reviews, only: [:create, :destroy]
-      resource :requests, only: [:create, :destroy]
-    end
-    get "/map", to: "users#map", as: "map"
-  end
+  resources :users
+  resources :commutes
+  resources :places, only: [:create, :destroy, :show, :index]
+  resource :reviews, only: [:create, :destroy]
+  resource :requests, only: [:create, :destroy]
+
+  get "/map", to: "users#map", as: "map"
+
 
 end
