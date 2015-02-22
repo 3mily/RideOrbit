0ebae33a8407a3e135e1de44a9df39d6486e19d4
Gabriel Leung <gabriel.leung.607@gmail.com>
2015-02-22 14:04:47 -0800

    login page

--- app/controllers/sessions_controller.rb
+++ app/controllers/sessions_controller.rb
@@ -1,2 +1,23 @@
 class SessionsController < ApplicationController
+
+
+  def new
+  end
+
+  def create
+    user = User.find_by(email: params[:email])
+
+    if user && user.authenticate(params[:password])
+      session[:user_id] = user.id
+      redirect_to root_path
+    else
+      flash.now[:alert] = "Log in failed :("
+      render :new
+    end
+  end
+
+  def destroy
+    session[:user_id] = nil
+    redirect_to root_path, notice: "Tsuus!"
+  end
 end
