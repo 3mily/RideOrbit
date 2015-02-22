5da1326dad5e3327b5df5465369e7c8fbb22e593
Gabriel Leung <gabriel.leung.607@gmail.com>
2015-02-22 14:12:15 -0800

    remove drive column migration

--- db/schema.rb
+++ db/schema.rb
@@ -11,7 +11,7 @@
 #
 # It's strongly recommended that you check this file into your version control system.
 
-ActiveRecord::Schema.define(version: 20150220234810) do
+ActiveRecord::Schema.define(version: 20150222221017) do
 
   # These are extensions that must be enabled in order to support this database
   enable_extension "plpgsql"
@@ -86,7 +86,6 @@ ActiveRecord::Schema.define(version: 20150220234810) do
     t.string   "email"
     t.string   "phone"
     t.string   "password_digest"
-    t.boolean  "is_driver"
     t.string   "linkedin"
     t.float    "rating"
     t.string   "picture"
