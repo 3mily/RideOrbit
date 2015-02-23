7863abe05562804e134e6771a07034171a634763
Emily <emrcraig@gmail.com>
2015-02-22 20:11:55 -0800

    create forms for driver commute and passenger commute

--- Gemfile
+++ Gemfile
@@ -44,7 +44,6 @@ gem 'sass'
 # Use Capistrano for deployment
 # gem 'capistrano-rails', group: :development
 
-gem "combined_time_select", "~> 1.0.1"
 
 group :development, :test do
   # Call 'byebug' anywhere in the code to stop execution and get a debugger console
--- app/controllers/commutes_controller.rb
+++ app/controllers/commutes_controller.rb
@@ -3,15 +3,36 @@ class CommutesController < ApplicationController
   end
 
   def new
-    @current_user = current_user
-    hour = params[:hour].to_i
-    minutes = params[:minutes]
-
-    if params[:am_pm] == "12"
-      hour += 12
-    end
-
     @driver_commute = Drivercommute.new
+    @driver_commute.user_id = current_user.id
+    
     @passenger_commute = Passengercommute.new
+    @passenger_commute.user_id = current_user.id
   end
+
+
+
+
+
+
+
+  #helper methods
+  def driver_commute_params
+    params.require(:driver_commute).permit(
+      :car_type, 
+      :seats_available, 
+      :driver_origin, 
+      :driver_destination, 
+      :driver_arrival_time
+    )
+  end
+
+  def passenger_commute_params
+    params.require(:passenger_commute).permit(
+      :passenger_origin, 
+      :passenger_destination, 
+      :passenger_arrival_time
+    )
+  end
+
 end
