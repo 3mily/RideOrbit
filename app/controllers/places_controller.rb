d58b31c1ef9ebdbd0d920e012f6ed3121833d5aa
Emily <emrcraig@gmail.com>
2015-02-23 19:45:41 -0800

    fix cross_street_points so they are not arrays

--- app/controllers/places_controller.rb
+++ app/controllers/places_controller.rb
@@ -8,6 +8,7 @@ class PlacesController < ApplicationController
 
   def create
     @place = Place.new(place_params)
+    binding.pry
     @place.user_id = current_user.id
     if @place.save
       flash[:notice] = "New place added!"
