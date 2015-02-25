4a67285b3b65070f95a018ac7497bd0404077b5d
Emily <emrcraig@gmail.com>
2015-02-24 21:21:48 -0800

    add autocomplete to places form

--- app/controllers/passengercommutes_controller.rb
+++ app/controllers/passengercommutes_controller.rb
@@ -5,8 +5,6 @@ class PassengercommutesController < ApplicationController
   def create    
     @passenger_commute = Passengercommute.new(passenger_commute_params)
     @passenger_commute.user_id = current_user.id
-    @passenger_commute.origin = nil
-    @passenger_commute.destination = nil
     @passenger_commute.save
 
     redirect_to commuteslist_path
