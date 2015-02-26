a54236226709783ee5a4cde2adc1d482f84fe187
Emily <emrcraig@gmail.com>
2015-02-26 14:47:35 -0800

    wire the  passenger commute form

--- app/controllers/drivercommutes_controller.rb
+++ app/controllers/drivercommutes_controller.rb
@@ -28,17 +28,15 @@ class DrivercommutesController < ApplicationController
 
   def driver_origin_params
     driver_origin_data = params["driver_origin"].split(",")
-    driver_origin_point = driver_origin_data[-2]+", "+driver_origin_data[-1]
-    driver_origin_name = driver_origin_data[0]
-    @driver_commute.origin_name = driver_origin_name
+    driver_origin_point = driver_origin_data[-2] + ", " + driver_origin_data[-1]
+    @driver_commute.origin_name = driver_origin_data[0]
     @driver_commute.origin = driver_origin_point
   end
 
   def driver_destination_params
     driver_destination_data = params["driver_destination"].split(",")
     driver_destination_point = driver_destination_data[-2]+", "+driver_destination_data[-1]
-    driver_destination_name = driver_destination_data[0]
-    @driver_commute.destination_name = driver_destination_name
+    @driver_commute.destination_name = driver_destination_data[0]
     @driver_commute.destination = driver_destination_point
   end
 
--- app/controllers/passengercommutes_controller.rb
+++ app/controllers/passengercommutes_controller.rb
@@ -6,6 +6,8 @@ class PassengercommutesController < ApplicationController
     @passenger_commute = Passengercommute.new(passenger_commute_params)
     @passenger_commute.user_id = current_user.id
     @passenger_commute.user_info = current_user
+    passenger_origin_params
+    passenger_destination_params
     @passenger_commute.save
 
     redirect_to commuteslist_path
@@ -22,10 +24,22 @@ class PassengercommutesController < ApplicationController
   #helper methods
   def passenger_commute_params
     params.require(:passengercommute).permit(
-      :origin, 
-      :destination, 
       :arrival_time
     )
+  end
+
+  def passenger_origin_params
+    passenger_origin_data = params["passenger_origin"].split(",")
+    passenger_origin_point = passenger_origin_data[-2] + ", " + passenger_origin_data[-1]
+    @passenger_commute.origin_name = passenger_origin_data[0]
+    @passenger_commute.origin = passenger_origin_point
+  end
+
+  def passenger_destination_params
+    passenger_destination_data = params["passenger_destination"].split(",")
+    passenger_destination_point = passenger_destination_data[-2] + ", " + passenger_destination_data[-1]
+    @passenger_commute.destination_name = passenger_destination_data[0]
+    @passenger_commute.destination = passenger_destination_point
   end
 
 end
