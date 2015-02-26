2720365f1d05ac4d048eda670260b9f98d2ef97b
Emily <emrcraig@gmail.com>
2015-02-26 11:18:05 -0800

    add flash notice, append div to page on form submit

--- app/controllers/drivercommutes_controller.rb
+++ app/controllers/drivercommutes_controller.rb
@@ -6,9 +6,8 @@ class DrivercommutesController < ApplicationController
   def create
     @driver_commute = Drivercommute.new(driver_commute_params)
     @driver_commute.user_id = current_user.id
-    # binding.pry
-    # @driver_commute.origin = nil
-    # @driver_commute.destination = nil
+    @driver_commute.origin_name =
+    @driver_commute.destination_name = 
     @driver_commute.save
     redirect_to commuteslist_path
   end
