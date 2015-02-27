4838e9e8dc4b9bd7f8f19ab550e00d6ecb18c9d6
Gabriel Leung <gabriel.leung.607@gmail.com>
2015-02-27 15:26:44 -0800

    able to send commute id through ajax

--- app/controllers/api_passenger_commutes_controller.rb
+++ app/controllers/api_passenger_commutes_controller.rb
@@ -1,7 +1,12 @@
 
 class ApiPassengerCommutesController < ApplicationController
   def retrieve
+    binding.pry
     @passengercommutes = Passengercommute.all
     render json: @passengercommutes
   end
 end
+
+
+    # @passengercommutes = Passengercommute.within_origin(current_user.id)
+    # binding.pry
--- app/models/passengercommute.rb
+++ app/models/passengercommute.rb
@@ -1,6 +1,6 @@
 class Passengercommute < ActiveRecord::Base
   belongs_to :drivercommute
   #Idk if interpolating something into the scope works that way btw, need to test later -gab
-  scope :within_origin, ->(current_user_id) {where("origin <@ CIRCLE((SELECT origin FROM drivercommutes WHERE user_id = ?), 0.015)", current_user_id)}
-  scope :within_destination, ->(current_user_id) {where("destination <@ CIRCLE((SELECT destination FROM drivercommutes WHERE user_id = ?), 0.0015)", current_user_id)}
+  scope :within_origin, ->(current_user_id) {where("origin <@ CIRCLE((SELECT origin FROM drivercommutes WHERE drivercommutes.id = ?), ?)", current_user_id, 0.015)}
+  scope :within_destination, ->(current_user_id) {where("destination <@ CIRCLE((SELECT destination FROM drivercommutes WHERE user_id = ?), ?)", current_user_id, 0.015)}
 end
