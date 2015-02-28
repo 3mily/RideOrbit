cf03cc9eecbdd46324c51bb0d2e21865217a43a4
Gabriel Leung <gabriel.leung.607@gmail.com>
2015-02-27 17:34:38 -0800

    refactor apipassengers ctrller

--- app/controllers/api_passenger_commutes_controller.rb
+++ app/controllers/api_passenger_commutes_controller.rb
@@ -3,8 +3,7 @@ class ApiPassengerCommutesController < ApplicationController
   def retrieve
     # binding.pry
     # @passengercommutes = Passengercommute.all
-    @passengercommutes = Passengercommute.within_origin(params["commute_id"])
-    @passengercommutes = @passengercommutes.within_destination(params["commute_id"])
+    @passengercommutes = Passengercommute.within_origin(params["commute_id"]).within_destination(params["commute_id"])
     # binding.pry
     render json: @passengercommutes
   end
