8234dc331caecc878934f4fd119606145566fe50
Gabriel Leung <gabriel.leung.607@gmail.com>
2015-02-27 17:36:34 -0800

    cleaned up contrller

--- app/controllers/api_passenger_commutes_controller.rb
+++ app/controllers/api_passenger_commutes_controller.rb
@@ -1,14 +1,8 @@
 
 class ApiPassengerCommutesController < ApplicationController
   def retrieve
-    # binding.pry
-    # @passengercommutes = Passengercommute.all
     @passengercommutes = Passengercommute.within_origin(params["commute_id"]).within_destination(params["commute_id"])
-    # binding.pry
     render json: @passengercommutes
   end
 end
 
-
-
-    # binding.pry
