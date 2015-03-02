9ca910c4913de0d14489cab40a3e4116a06e96cd
Emily <emrcraig@gmail.com>
2015-03-02 13:13:07 -0800

    fix button link to new commutes

--- app/controllers/twilio_controller.rb
+++ app/controllers/twilio_controller.rb
@@ -1,2 +1,21 @@
 class TwilioController < ApplicationController
+
+  def create
+
+
 end
+
+
+  function updateDriverCommute(){
+    $.ajax({
+      url: "/twilio",
+      type: "PUT",
+      data: params,
+      error: function(xhr,status,thrownError){
+        console.log("it didnt save or work or something oh noes", thrownError);
+      },
+      success: function(response){
+        console.log("success - twilio text send"t
+      }
+    });
+  }
