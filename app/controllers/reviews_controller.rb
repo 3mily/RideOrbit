9c9150c988e70004d7e2561020a1ddd852b3e675
Hyeunjoon Kim <hyeunjoon@Hyeunjoons-MacBook-Pro.local>
2015-03-02 19:10:20 -0800

    users can delete reviews that they wrote

--- app/controllers/reviews_controller.rb
+++ app/controllers/reviews_controller.rb
@@ -8,6 +8,13 @@ class ReviewsController < ApplicationController
     end
   end
 
+  def destroy
+    @review = Review.where(reviewer_id: params[:reviewer_id]).first
+    if @review.destroy
+      redirect_to user_path(@review.reviewee_id)    
+    end
+  end
+
   #helper
   def review_params
     params.require(:review).permit(:drivercommute_id, :passengercommute_id, :reviewee_id, :reviewer_id, :rating, :body)
