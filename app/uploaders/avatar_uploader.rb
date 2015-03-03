09676de1738d8f377c378d434dcbfac0a3587974
Hyeunjoon Kim <hyeunjoon@Hyeunjoons-MacBook-Pro.local>
2015-03-02 20:39:37 -0800

    carrierwave images are automatically resized

--- app/uploaders/avatar_uploader.rb
+++ app/uploaders/avatar_uploader.rb
@@ -4,7 +4,8 @@ class AvatarUploader < CarrierWave::Uploader::Base
 
   # Include RMagick or MiniMagick support:
   # include CarrierWave::RMagick
-  # include CarrierWave::MiniMagick
+  include CarrierWave::MiniMagick
+  process :resize_to_fit => [200, 200]
 
   # Choose what kind of storage to use for this uploader:
   storage :file
