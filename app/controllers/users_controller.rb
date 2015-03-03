36014dadc41130fc91f6fd7cadbec7e66576698e
Hyeunjoon Kim <hyeunjoon@Hyeunjoons-MacBook-Pro.local>
2015-03-02 19:52:31 -0800

    added carrierwave, still need to test

--- app/controllers/users_controller.rb
+++ app/controllers/users_controller.rb
@@ -11,7 +11,6 @@ class UsersController < ApplicationController
   end
 
   def create
-    binding.pry
     @user = User.new(user_params)
     if @user.save
       session[:user_id] = @user.id
@@ -31,6 +30,6 @@ class UsersController < ApplicationController
 
   # helper method
   def user_params
-    params.require(:user).permit(:firstname, :lastname, :email, :phone, :password, :password_confirmation)
+    params.require(:user).permit(:firstname, :lastname, :email, :phone, :password, :password_confirmation, :image)
   end
 end
