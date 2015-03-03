014486ed72d72870c6c39c32adf89accb90bcb14
Emily <emrcraig@gmail.com>
2015-03-02 19:49:25 -0800

    change phone input

--- app/controllers/users_controller.rb
+++ app/controllers/users_controller.rb
@@ -10,8 +10,9 @@ class UsersController < ApplicationController
   end
 
   def create
-    binding.pry
     @user = User.new(user_params)
+    @user.phone = "+"+params["phone1"]+params["phone2"]+params["phone3"]
+
     if @user.save
       session[:user_id] = @user.id
       flash[:notice] = "Welcome aboard, #{@user.firstname}! Now, add some places!"
@@ -29,6 +30,6 @@ class UsersController < ApplicationController
 
   # helper method
   def user_params
-    params.require(:user).permit(:firstname, :lastname, :email, :phone, :password, :password_confirmation)
+    params.require(:user).permit(:firstname, :lastname, :email, :password, :password_confirmation)
   end
 end
