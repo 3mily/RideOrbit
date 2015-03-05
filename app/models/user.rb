13f731ef94767d20130a7903830ba815b21a658f
Gabriel Leung <gabriel.leung.607@gmail.com>
2015-03-05 11:39:53 -0800

    faked seed data

--- app/models/user.rb
+++ app/models/user.rb
@@ -6,7 +6,7 @@ class User < ActiveRecord::Base
   has_secure_password
 
   validates :email, presence: true, uniqueness: true
-  validates :phone, presence: true, uniqueness: true
+  # validates :phone, presence: true, uniqueness: true
   validates :firstname, presence: true
   validates :lastname, presence: true
   validates :password, length: { in: 1..20}, on: :create
--- db/seeds.rb
+++ db/seeds.rb
@@ -7,53 +7,58 @@
 #   Mayor.create(name: 'Emanuel', city: cities.first)
 phone_array = ["+6048020926", "+7783231717", "+7789776806"]
 
-1.upto(9) do |i|
-  firstname = Faker::Name.first_name
-  lastname = Faker::Name.last_name
-  industry_array = ["agriculture","energy","entertainment","finance","government","health/medical","hospitality","marketing/communications","non-profit","publishing","science","technology","transportation"]
-  industry = industry_array[rand(0...industry_array.length)]
-  user = User.new(firstname: firstname, lastname: lastname, phone: "604802092#{i}", password:"pwpwpw", email:"email#{i}@email.io", industry: industry, bio:"Hi my name is #{firstname} let's go for a ride!!!") 
-  user.picture = File.open(Rails.root.join('public', 'uploads', 'user', 'picture', '11', 'hyeunny.jpeg'))
-  user.save!
-end
+User.create(
+  firstname: "HJ",
+  lastname: "Kim",
+  email: "hj@rideorbit.com",
+  phone:"+16048020926",
+  password:"12345",
+  industry: "agriculture",
+  bio: "I raise cows to cook and make into steaks. Mmmmmm."
+  )
 
+User.create(
+  firstname: "Emily",
+  lastname: "Craig",
+  email: "emily@rideorbit.com",
+  phone:"+17789776806",
+  password:"12345",
+  industry: "technology",
+  bio: "Tacos are love, tacos are life. I make websites look awesome!"
+  )
+
+User.create(
+  firstname: "Gabby",
+  lastname: "Leung",
+  email: "gabby@rideorbit.com",
+  phone:"+17783231717",
+  password:"12345",
+  industry: "technology",
+  bio: "Taco is life, taco is love. Also building websites is awesome!"
+  )
+
+gabby = User.find(3)
+emily = User.find(2)
+hj = User.find(1)
+hj.picture = File.open(Rails.root.join('public', 'uploads', 'user', 'picture', '11', 'hyeunny.jpeg'))
+gabby.picture = File.open(Rails.root.join('public', 'uploads', 'user', 'picture', '1', 'profile_pic.png'))
+emily.picture = File.open(Rails.root.join('public', 'uploads', 'user', 'picture', '1', 'emily.jpg'))
+gabby.save!
+emily.save!
+hj.save!
 
 Place.create(
-  user_id:1, 
+  user_id:3,
   name: "Home",
   cross_street:"41st and Cambie (Oakridge Area)", 
   cross_street_point: "49.233343, -123.116658"
   )
 
 Place.create(
-  user_id:1, 
+  user_id:3, 
   name: "Work",
   cross_street:"Abbott and W Hastings", 
   cross_street_point: "49.282251, -123.107195"
-  )
-
-Passengercommute.create(
-  user_id: 1,
-  origin: "49.233343, -123.116658",
-  origin_name: "Home - 41st and Cambie (Oakridge Area)",
-  destination: "49.282251, -123.107195",
-  destination_name: "Work - Abbott and W Hastings (Downtown)",
-  arrival_time: "2000-01-01 02:30:00",
-  days: ["Mon","Tues","Wed"],
-  user_info: {name: User.find(1).firstname, picture: User.find(1).picture, industry: User.find(1).industry, phone: phone_array[rand(0..2)]}
-  )
-
-Drivercommute.create(
-  user_id: 1,
-  car_type: "Beetle",
-  origin_name: "Home - 41st and Cambie (Oakridge Area)",
-  origin: "49.233343, -123.116658",
-  destination_name: "Work - Abbott and W Hastings (Downtown)",
-  destination: "49.282251, -123.107195",
-  arrival_time: "2000-01-01 02:30:00",
-  seats_available: 3,
-  days: ["Mon","Tues","Wed"],
-  user_info: {name: User.find(1).firstname, picture: User.find(1).picture, industry: User.find(1).industry, phone: phone_array[rand(0..2)]}
   )
 
 Place.create(
@@ -70,63 +75,125 @@ Place.create(
   cross_street_point: "49.276960, -123.128770"
   )
 
-Passengercommute.create(
-  user_id: 2,
-  origin: "49.219391, -123.128614",
-  origin_name: "Home - 57th and Oak",
-  destination: "49.276960, -123.128770",
-  destination_name: "Howe and Drake",
-  arrival_time: "2000-01-01 02:30:00",
-  days: ["Mon","Tues","Wed"],
-  user_info: {name: User.find(2).firstname, picture: User.find(2).picture, industry: User.find(2).industry, phone: phone_array[rand(0..2)]}
-  )
-
-Drivercommute.create(
-  user_id: 2,
-  origin: "49.219391, -123.128614",
-  origin_name: "Home - 57th and Oak",
-  destination: "49.276960, -123.128770",
-  destination_name: "Howe and Drake",
-  arrival_time: "2000-01-01 02:30:00",
-  days: ["Mon","Tues","Wed"],
-  user_info: {name: User.find(2).firstname, picture: User.find(2).picture, industry: User.find(2).industry, phone: phone_array[rand(0..2)]}
-  )
-
-  Place.create(
-  user_id:3, 
+Place.create(
+  user_id:1,
   name: "Home",
-  cross_street:"Bute and Harwood", 
-  cross_street_point: "49.280591, -123.135468"
+  cross_street:"39th Ave and Manitoba St", 
+  cross_street_point: "49.235583, -123.108028"
   )
 
 Place.create(
-  user_id:3, 
+  user_id:1, 
   name: "Work",
-  cross_street:"Burrard and Pender", 
-  cross_street_point: "49.286771, -123.117788"
+  cross_street:"Hamilton and Robson St", 
+  cross_street_point: "49.278860, -123.115923"
   )
 
 Passengercommute.create(
   user_id: 3,
-  origin: "49.280591, -123.135468",
-  origin_name: "Bute and Harwood",
-  destination: "49.286771, -123.117788",
-  destination_name: "Burrard and Pender",
+  origin: "49.233343, -123.116658",
+  origin_name: "Home - 41st and Cambie (Oakridge Area)",
+  destination: "49.282251, -123.107195",
+  destination_name: "Work - Abbott and W Hastings (Downtown)",
   arrival_time: "2000-01-01 02:30:00",
   days: ["Mon","Tues","Wed"],
-  user_info: {name: User.find(3).firstname, picture: User.find(3).picture, industry: User.find(3).industry, phone: phone_array[rand(0..2)]}
+  user_info: {name: User.find(3).firstname, picture: User.find(3).picture, industry: User.find(3).industry, phone: User.find(3).phone, id: 3}
   )
 
 Drivercommute.create(
-  user_id: 3,
-  origin: "49.280591, -123.135468",
-  origin_name: "Bute and Harwood",
-  destination: "49.286771, -123.117788",
-  destination_name: "Burrard and Pender",
+  user_id: 1,
+  origin: "49.235583, -123.108028",
+  origin_name: "Home - 39th Ave and Manitoba St",
+  destination: "49.278860, -123.115923",
+  destination_name: "Work - Hamilton and Robson St",
   arrival_time: "2000-01-01 02:30:00",
   days: ["Mon","Tues","Wed"],
-  user_info: {name: User.find(3).firstname, picture: User.find(3).picture, industry: User.find(3).industry, phone: phone_array[rand(0..2)]}
+  user_info: {name: User.find(1).firstname, picture: User.find(1).picture, industry: User.find(1).industry, phone: User.find(1).phone, id: 1}
   )
+
+Drivercommute.create(
+  user_id: 2,
+  origin: "49.219391, -123.128614",
+  origin_name: "Home - 57th and Oak",
+  destination: "49.276960, -123.128770",
+  destination_name: "Work - Howe and Drake",
+  arrival_time: "2000-01-01 02:30:00",
+  days: ["Mon","Tues","Wed"],
+  user_info: {name: User.find(2).firstname, picture: User.find(2).picture, industry: User.find(2).industry, phone: User.find(2).phone, id: 2}
+  )
+
+
+
+# Drivercommute.create(
+#   user_id: 1,
+#   car_type: "Beetle",
+#   origin_name: "Home - 41st and Cambie (Oakridge Area)",
+#   origin: "49.233343, -123.116658",
+#   destination_name: "Work - Abbott and W Hastings (Downtown)",
+#   destination: "49.282251, -123.107195",
+#   arrival_time: "2000-01-01 02:30:00",
+#   seats_available: 3,
+#   days: ["Mon","Tues","Wed"],
+#   user_info: {name: User.find(1).firstname, picture: User.find(1).picture, industry: User.find(1).industry, phone: phone_array[rand(0..2)]}
+#   )
+
+
+# Passengercommute.create(
+#   user_id: 2,
+#   origin: "49.219391, -123.128614",
+#   origin_name: "Home - 57th and Oak",
+#   destination: "49.276960, -123.128770",
+#   destination_name: "Howe and Drake",
+#   arrival_time: "2000-01-01 02:30:00",
+#   days: ["Mon","Tues","Wed"],
+#   user_info: {name: User.find(2).firstname, picture: User.find(2).picture, industry: User.find(2).industry, phone: phone_array[rand(0..2)]}
+#   )
+
+1.upto(9) do |i|
+  firstname = Faker::Name.first_name
+  lastname = Faker::Name.last_name
+  industry_array = ["agriculture","energy","entertainment","finance","government","health/medical","hospitality","marketing/communications","non-profit","publishing","science","technology","transportation"]
+  industry = industry_array[rand(0...industry_array.length)]
+  user = User.new(firstname: firstname, lastname: lastname, phone: "604802092#{i}", password:"pwpwpw", email:"email#{i}@email.io", industry: industry, bio:"Hi my name is #{firstname} let's go for a ride!!!") 
+  user.picture = File.open(Rails.root.join('public', 'uploads', 'user', 'picture', '11', 'hyeunny.jpeg'))
+  user.save!
+end
+
+#   Place.create(
+#   user_id:3, 
+#   name: "Home",
+#   cross_street:"Bute and Harwood", 
+#   cross_street_point: "49.280591, -123.135468"
+#   )
+
+# Place.create(
+#   user_id:3, 
+#   name: "Work",
+#   cross_street:"Burrard and Pender", 
+#   cross_street_point: "49.286771, -123.117788"
+#   )
+
+# Passengercommute.create(
+#   user_id: 3,
+#   origin: "49.280591, -123.135468",
+#   origin_name: "Bute and Harwood",
+#   destination: "49.286771, -123.117788",
+#   destination_name: "Burrard and Pender",
+#   arrival_time: "2000-01-01 02:30:00",
+#   days: ["Mon","Tues","Wed"],
+#   user_info: {name: User.find(3).firstname, picture: User.find(3).picture, industry: User.find(3).industry, phone: phone_array[rand(0..2)]}
+#   )
+
+# Drivercommute.create(
+#   user_id: 3,
+#   origin: "49.280591, -123.135468",
+#   origin_name: "Bute and Harwood",
+#   destination: "49.286771, -123.117788",
+#   destination_name: "Burrard and Pender",
+#   arrival_time: "2000-01-01 02:30:00",
+#   days: ["Mon","Tues","Wed"],
+#   user_info: {name: User.find(3).firstname, picture: User.find(3).picture, industry: User.find(3).industry, phone: phone_array[rand(0..2)]}
+#   )
 
 
 Place.create(
