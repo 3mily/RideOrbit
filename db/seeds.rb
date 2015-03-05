778de8b8a1836202c933fece4f49348a92efaffb
Hyeunjoon Kim <hyeunjoon.kim@mail.mcgill.ca>
2015-03-04 17:38:11 -0800

    new seeds

--- db/seeds.rb
+++ db/seeds.rb
@@ -26,390 +26,318 @@ Place.create(
 Place.create(
   user_id:1, 
   name: "Work",
-  cross_street:"Abbott and W Hastings (Downtown)", 
-  cross_street_point: "49.2820030, -123.1072000"
+  cross_street:"Abbott and W Hastings", 
+  cross_street_point: "49.282251, -123.107195"
   )
 
-Place.create(
-  user_id:2, 
-  name: "Home",
-  cross_street:"49th and Oak (Vancouver)", 
-  cross_street_point: "49.226557, -123.128330"
-  )
-
-Place.create(
-  user_id:2, 
-  name: "Work",
-  cross_street:"King Edward and Cambie (Vancouver)", 
-  cross_street_point: "49.248910, -123.115528"
-  )                     
-
-Place.create(
-  user_id:3, 
-  name: "Home",
-  cross_street:"No 3 Rd and Cooke (Richmond)", 
-  cross_street_point: "49.166522, -123.136471"
-  )
-
-Place.create(
-  user_id:3, 
-  name: "Work",
-  cross_street:"Seymour and Dunsmuir (Downtown)", 
-  cross_street_point: "49.283003, -123.115422"
-  )
-
-Place.create(
-  user_id:4, 
-  name: "Home",
-  cross_street:"Westminster Hwy and Garden City Rd (Richmond)", 
-  cross_street_point: "49.170068, -123.124516"
-  )
-
-Place.create(
-  user_id:4, 
-  name: "Work",
-  cross_street:"Nicola and Robson (Downtown)", 
-  cross_street_point: "49.288682, -123.131102"
-  )
-
-Place.create(
-  user_id:4, 
-  name: "Work 2",
-  cross_street:"Agronomy Rd and E Mall (UBC)", 
-  cross_street_point: "49.261352, -123.246944"
-  )
-
-Place.create(
-  user_id:5, 
-  name: "Home",
-  cross_street:"Cooke and Garden City Rd (Richmond)", 
-  cross_street_point: "49.166452, -123.124795"
-  )
-
-Place.create(
-  user_id:5, 
-  name: "Work",
-  cross_street:"Wesbrook Mall and Thunderbird Blvd (UBC)", 
-  cross_street_point: "49.261669, -123.241450"
-  )
-
-Place.create(
-  user_id:5, 
-  name: "Work 2",
-  cross_street:"Seymour and Smithe (Downtown)", 
-  cross_street_point: "49.279679, -123.120421"
-  )
-Place.create(
-  user_id:6, 
-  name: "Home",
-  cross_street:"Como Lake Ave and Blue Mountain (Coquitlam)", 
-  cross_street_point: "49.263547, -122.868766"
-  )
-
-Place.create(
-  user_id:6, 
-  name: "Work",
-  cross_street:"37th Ave and Kersland Dr (Oakridge Area)", 
-  cross_street_point: "49.237140, -123.114471"
-  )
-
-Place.create(
-  user_id:7, 
-  name: "Home",
-  cross_street:"Colinet and Cottonwood (Coquitlam)", 
-  cross_street_point: "49.258137, -122.867482"
-  )
-
-Place.create(
-  user_id:7, 
-  name: "Work",
-  cross_street:"Lyndhurst and Willoughby (Coquitlam)", 
-  cross_street_point: "49.258856, -122.895429"
-  )
-
-Place.create(
-  user_id:8, 
-  name: "Home",
-  cross_street:"St Albans Rd and Jones Rd (Richmond)", 
-  cross_street_point: "49.157374, -123.130940"
-  )
-
-Place.create(
-  user_id:8, 
-  name: "Work",
-  cross_street:"Como Lake Ave and Crestwood Dr (Coquitlam)", 
-  cross_street_point: "49.263468, -122.852723"
-  )
-
-Place.create(
-  user_id:9, 
-  name: "Home",
-  cross_street:"Cornish and 68th Ave (Vancouver)", 
-  cross_street_point: "49.210531, -123.142071"
-  )
-
-Place.create(
-  user_id:9, 
-  name: "Work",
-  cross_street:"E Mall and Thunderbird Blvd", 
-  cross_street_point: "49.260103, -123.245706"
-  )
-
-Place.create(
-  user_id:10, 
-  name: "Home",
-  cross_street:"Nelson Ave and Maitland St", 
-  cross_street_point: "49.166522, -123.136471"
-  )
-
-Place.create(
-  user_id:10, 
-  name: "Work",
-  cross_street:"Schoolhouse and Austin Ave", 
-  cross_street_point: "49.283003, -123.115422"
-  )
-# I won't put in a drivercommute id in yet because that association
-# should get made when both parties agree
 Passengercommute.create(
   user_id: 1,
   origin: "49.233343, -123.116658",
   origin_name: "Home - 41st and Cambie (Oakridge Area)",
-  destination: "49.2820030, -123.1072000",
+  destination: "49.282251, -123.107195",
   destination_name: "Work - Abbott and W Hastings (Downtown)",
   arrival_time: "2000-01-01 02:30:00",
   days: ["Mon","Tues","Wed"],
   user_info: {name: "User 1 P Commute", email:"test@test.ca", phone: "604-802-1234"}
   )
-Passengercommute.create(
-  user_id: 2,
-  origin_name: "Home - 49th and Oak (Vancouver)",
-  origin: "49.226557, -123.128330",
-  destination_name: "Work - King Edward and Cambie (Vancouver)",
-  destination: "49.248910, -123.115528",
-  arrival_time: "2000-01-01 02:30:00",
-  days: ["Mon","Tues","Wed"],
-  user_info: {name: "User 2 P Commute", email:"test@test.ca", phone: "604-802-1234"}
-  )
-Passengercommute.create(
-  user_id: 3,
-  origin: "49.166522, -123.136471",
-  origin_name: "Home - No 3 Rd and Cooke (Richmond)",
-  destination: "49.283003, -123.115422",
-  destination_name: "Work - Seymour and Dunsmuir (Downtown)",
-  arrival_time: "2000-01-01 02:30:00",
-  days: ["Mon","Tues","Wed"],
-  user_info: {name: "User 3 P Commute", email:"special@test.ca", phone: "604-802-1234"}
-  )
-Passengercommute.create(
-  user_id: 4,
-  origin: "49.170068, -123.124516",
-  origin_name: "Home - Westminster Hwy and Garden City Rd (Richmond)",
-  destination: "49.288682, -123.131102",
-  destination_name: "Work - Nicola and Robson (Downtown)",
-  arrival_time: "2000-01-01 02:30:00",
-  days: ["Mon","Tues","Wed"],
-  user_info: {name: "User 4 P Commute", email:"test@test.ca", phone: "604-802-1234"}
 
-  )
-Passengercommute.create(
-  user_id: 5,
-  origin_name:"Home - Cooke and Garden City Rd (Richmond)",
-  origin: "49.166452, -123.124795",
-  destination_name: "Work - Wesbrook Mall and Thunderbird Blvd (UBC)",
-  destination: "49.261669,-123.241450",
-  arrival_time: "2000-01-01 02:30:00",
-  days: ["Mon","Tues","Wed"],
-  user_info: {name: "User 5 P Commute", email:"test@test.ca", phone: "604-802-1234"}
-  )
-Passengercommute.create(
-  user_id: 5,
-  origin_name: "Work - Wesbrook Mall and Thunderbird Blvd (UBC)",
-  origin: "49.261669, -123.241450",
-  destination_name: "Work 2 - Seymour and Smithe (Downtown)",
-  destination: "49.279679, -123.120421",
-  arrival_time: "2000-01-01 02:30:00",
-  days: ["Mon","Tues","Wed"],
-  user_info: {name: "User 5 P Commute", email:"test@test.ca", phone: "604-802-1234"}
-  )
-Passengercommute.create(
-  user_id: 6,
-  origin_name: "Home - Como Lake Ave and Blue Mountain (Coquitlam)",
-  origin: "49.263547, -122.868766",
-  destination_name: "Work - 37th Ave and Kersland Dr (Oakridge Area)",
-  destination: "49.237140, -123.114471",
-  arrival_time: "2000-01-01 02:30:00",
-  days: ["Mon","Tues","Wed"],
-  user_info: {name: "User 6 P Commute", email:"test@test.ca", phone: "604-802-1234"}
-  )
-Passengercommute.create(
-  user_id: 7,
-  origin_name: "Home - Colinet and Cottonwood (Coquitlam)",
-  origin: "49.258137, -122.867482",
-  destination_name: "Work - Lyndhurst and Willoughby (Coquitlam)",
-  destination: "49.258856, -122.895429",
-  arrival_time: "2000-01-01 02:30:00",
-  days: ["Mon","Tues","Wed"],
-  user_info: {name: "User 7 P Commute", email:"test@test.ca", phone: "604-802-1234"}
-  )
-Passengercommute.create(
-  user_id: 8,
-  origin_name: "Home - St Albans Rd and Jones Rd (Richmond)",
-  origin: "49.157374, -123.130940",
-  destination_name: "Work - Como Lake Ave and Crestwood Dr (Coquitlam)",
-  destination: "49.263468, -122.852723",
-  arrival_time: "2000-01-01 02:30:00",
-  days: ["Mon","Tues","Wed"],
-  user_info: {name: "User 8 P Commute", email:"test@test.ca", phone: "604-802-1234"}
-  )
-Passengercommute.create(
-  user_id: 9,
-  origin_name: "Home - Cornish and 68th Ave (Vancouver)",
-  origin: "49.210531, -123.142071",
-  destination_name: "Work - E Mall and Thunderbird Blvd",
-  destination: "49.260103, -123.245706",
-  arrival_time: "2000-01-01 02:30:00",
-  days: ["Mon","Tues","Wed"],
-  user_info: {name: "User 9 P Commute", email:"test@test.ca", phone: "604-802-1234"}
-  )
-Passengercommute.create(
-  user_id: 10,
-  origin_name: "Home - Nelson Ave and Maitland St",
-  origin: "49.166522, -123.136471",
-  destination_name: "Work - Schoolhouse and Austin Ave",
-  destination: "49.283003, -123.115422",
-  arrival_time: "2000-01-01 02:30:00",
-  days: ["Mon","Tues","Wed"],
-  user_info: {name: "User 10 P Commute", email:"test@test.ca", phone: "604-802-1234"}
-  )
 Drivercommute.create(
   user_id: 1,
-  car_type: "beetle",
+  car_type: "Beetle",
   origin_name: "Home - 41st and Cambie (Oakridge Area)",
   origin: "49.233343, -123.116658",
   destination_name: "Work - Abbott and W Hastings (Downtown)",
-  destination: "49.2820030, -123.1072000",
+  destination: "49.282251, -123.107195",
   arrival_time: "2000-01-01 02:30:00",
   seats_available: 3,
   days: ["Mon","Tues","Wed"],
   user_info: {name: "User 1 D Commute", email:"test@test.ca", phone: "604-802-1234"}
   )
+
+Place.create(
+  user_id:2, 
+  name: "Home",
+  cross_street:"57th and Oak", 
+  cross_street_point: "49.219391, -123.128614"
+  )
+
+Place.create(
+  user_id:2, 
+  name: "Work",
+  cross_street:"Howe and Drake", 
+  cross_street_point: "49.276960, -123.128770"
+  )
+
+Passengercommute.create(
+  user_id: 2,
+  origin: "49.219391, -123.128614",
+  origin_name: "Home - 57th and Oak",
+  destination: "49.276960, -123.128770",
+  destination_name: "Howe and Drake",
+  arrival_time: "2000-01-01 02:30:00",
+  days: ["Mon","Tues","Wed"],
+  user_info: {name: "User 1 P Commute", email:"test@test.ca", phone: "604-802-1234"}
+  )
+
 Drivercommute.create(
   user_id: 2,
-  car_type: "truck",
-  origin_name: "Home - 49th and Oak (Vancouver)",
-  origin: "49.226557, -123.128330",
-  destination_name: "Work - King Edward and Cambie (Vancouver)",
-  destination: "49.248910, -123.115528",
+  origin: "49.219391, -123.128614",
+  origin_name: "Home - 57th and Oak",
+  destination: "49.276960, -123.128770",
+  destination_name: "Howe and Drake",
   arrival_time: "2000-01-01 02:30:00",
-  seats_available: 3,
   days: ["Mon","Tues","Wed"],
-  user_info: {name: "User 2 D Commute", email:"test@test.ca", phone: "604-802-1234"}
+  user_info: {name: "User 1 P Commute", email:"test@test.ca", phone: "604-802-1234"}
   )
+
+Place.create(
+  user_id:3, 
+  name: "Home",
+  cross_street:"Bute and Harwood", 
+  cross_street_point: "49.280591, -123.135468"
+  )
+
+Place.create(
+  user_id:3, 
+  name: "Work",
+  cross_street:"Burrard and Pender", 
+  cross_street_point: "49.286771, -123.117788"
+  )
+
+Passengercommute.create(
+  user_id: 3,
+  origin: "49.280591, -123.135468",
+  origin_name: "Bute and Harwood",
+  destination: "49.286771, -123.117788",
+  destination_name: "Burrard and Pender",
+  arrival_time: "2000-01-01 02:30:00",
+  days: ["Mon","Tues","Wed"],
+  user_info: {name: "User 1 P Commute", email:"test@test.ca", phone: "604-802-1234"}
+  )
+
 Drivercommute.create(
   user_id: 3,
-  car_type: "beetle",
-  origin_name: "Home - No 3 Rd and Cooke (Richmond)",
-  origin: "49.166522, -123.136471",
-  destination_name: "Work - Seymour and Dunsmuir (Downtown)",
-  destination: "49.283003, -123.115422",
+  origin: "49.280591, -123.135468",
+  origin_name: "Bute and Harwood",
+  destination: "49.286771, -123.117788",
+  destination_name: "Burrard and Pender",
   arrival_time: "2000-01-01 02:30:00",
   days: ["Mon","Tues","Wed"],
-  user_info: {name: "User 3 D Commute", email:"test@test.ca", phone: "604-802-1234"}
+  user_info: {name: "User 1 P Commute", email:"test@test.ca", phone: "604-802-1234"}
   )
+
+
+Place.create(
+  user_id:4, 
+  name: "Home",
+  cross_street:"Commercial and Broadway", 
+  cross_street_point: "49.262892, -123.069641"
+  )
+
+Place.create(
+  user_id:4, 
+  name: "Work",
+  cross_street:"Quebec and 8th", 
+  cross_street_point: "49.263930, -123.102960"
+  )
+
+Passengercommute.create(
+  user_id: 4,
+  origin: "49.262892, -123.069641",
+  origin_name: "Commercial and Broadway",
+  destination: "49.263930, -123.102960",
+  destination_name: "Quebec and 8th",
+  arrival_time: "2000-01-01 02:30:00",
+  days: ["Mon","Tues","Wed"],
+  user_info: {name: "User 1 P Commute", email:"test@test.ca", phone: "604-802-1234"}
+  )
+
 Drivercommute.create(
   user_id: 4,
-  car_type: "SUV",
-  origin_name: "Home - Westminster Hwy and Garden City Rd (Richmond)",
-  origin: "49.170068, -123.124516",
-  destination_name: "Work - Nicola and Robson (Downtown)",
-  destination: "49.288682, -123.131102",
+  origin: "49.262892, -123.069641",
+  origin_name: "Commercial and Broadway",
+  destination: "49.263930, -123.102960",
+  destination_name: "Quebec and 8th",
   arrival_time: "2000-01-01 02:30:00",
-  seats_available: 3,
   days: ["Mon","Tues","Wed"],
-  user_info: {name: "User 4 D Commute", email:"test@test.ca", phone: "604-802-1234"}
+  user_info: {name: "User 1 P Commute", email:"test@test.ca", phone: "604-802-1234"}
   )
+
+Place.create(
+  user_id:5, 
+  name: "Home",
+  cross_street:"Holdom and Lougheed", 
+  cross_street_point: "49.264459, -122.981830"
+  )
+
+Place.create(
+  user_id:5, 
+  name: "Work",
+  cross_street:"Curtis and Sperling", 
+  cross_street_point: "49.275427, -122.964699"
+  )
+
+Passengercommute.create(
+  user_id: 5,
+  origin: "49.264459, -122.981830",
+  origin_name: "Holdom and Lougheed",
+  destination: "49.275427, -122.964699",
+  destination_name: "Curtis and Sperling",
+  arrival_time: "2000-01-01 02:30:00",
+  days: ["Mon","Tues","Wed"],
+  user_info: {name: "User 1 P Commute", email:"test@test.ca", phone: "604-802-1234"}
+  )
+
 Drivercommute.create(
   user_id: 5,
-  car_type: "sports car thing",
-  origin_name: "Home - Cooke and Garden City Rd (Richmond)",
-  origin: "49.166452, -123.124795",
-  destination_name: "Work - Wesbrook Mall and Thunderbird Blvd (UBC)",
-  destination: "49.261669, -123.241450",
+  origin: "49.264459, -122.981830",
+  origin_name: "Holdom and Lougheed",
+  destination: "49.275427, -122.964699",
+  destination_name: "Curtis and Sperling",
   arrival_time: "2000-01-01 02:30:00",
-  seats_available: 3,
   days: ["Mon","Tues","Wed"],
-  user_info: {name: "User 5 D Commute", email:"test@test.ca", phone: "604-802-1234"}
+  user_info: {name: "User 1 P Commute", email:"test@test.ca", phone: "604-802-1234"}
   )
-Drivercommute.create(
-  user_id: 5,
-  car_type: "sports car thing",
-  origin_name: "Work - Wesbrook Mall and Thunderbird Blvd (UBC)",
-  origin: "49.261669, -123.241450",
-  destination_name: "Work 2 - Seymour and Smithe (Downtown)",
-  destination: "49.279679, -123.120421",
+
+Place.create(
+  user_id:6, 
+  name: "Home",
+  cross_street:"Rumble and Buller", 
+  cross_street_point: "49.215050, -122.977552"
+  )
+
+Place.create(
+  user_id:6, 
+  name: "Work",
+  cross_street:"McKay and Central", 
+  cross_street_point: "49.226850, -123.005663"
+  )
+
+Passengercommute.create(
+  user_id: 6,
+  origin: "49.215050, -122.977552",
+  origin_name: "Rumble and Buller",
+  destination: "49.226850, -123.005663",
+  destination_name: "McKay and Central",
   arrival_time: "2000-01-01 02:30:00",
-  seats_available: 3,
   days: ["Mon","Tues","Wed"],
-  user_info: {name: "User 5 D Commute", email:"test@test.ca", phone: "604-802-1234"}
+  user_info: {name: "User 1 P Commute", email:"test@test.ca", phone: "604-802-1234"}
   )
+
 Drivercommute.create(
   user_id: 6,
-  car_type: "SUV",
-  origin_name: "Home - Como Lake Ave and Blue Mountain (Coquitlam)",
-  origin: "49.263547, -122.868766",
-  destination_name: "Work - 37th Ave and Kersland Dr (Oakridge Area)",
-  destination: "49.237140, -123.114471",
+  origin: "49.215050, -122.977552",
+  origin_name: "Rumble and Buller",
+  destination: "49.226850, -123.005663",
+  destination_name: "McKay and Central",
   arrival_time: "2000-01-01 02:30:00",
-  seats_available: 4,
   days: ["Mon","Tues","Wed"],
-  user_info: {name: "User 6 D Commute", email:"test@test.ca", phone: "604-802-1234"}
+  user_info: {name: "User 1 P Commute", email:"test@test.ca", phone: "604-802-1234"}
   )
+
+Place.create(
+  user_id:7, 
+  name: "Home",
+  cross_street:"Blundell and Garden City", 
+  cross_street_point: "49.155635, -123.125297"
+  )
+
+Place.create(
+  user_id:7, 
+  name: "Work",
+  cross_street:"No 3 and Cook", 
+  cross_street_point: "49.166760, -123.136480"
+  )
+
+Passengercommute.create(
+  user_id: 7,
+  origin: "49.155635, -123.125297",
+  origin_name: "Blundell and Garden City",
+  destination: "49.166760, -123.136480",
+  destination_name: "No 3 and Cook",
+  arrival_time: "2000-01-01 02:30:00",
+  days: ["Mon","Tues","Wed"],
+  user_info: {name: "User 1 P Commute", email:"test@test.ca", phone: "604-802-1234"}
+  )
+
 Drivercommute.create(
   user_id: 7,
-  car_type: "soccermomtruck",
-  origin_name: "Home - Colinet and Cottonwood (Coquitlam)",
-  origin: "49.258137, -122.867482",
-  destination_name: "Work - Lyndhurst and Willoughby (Coquitlam)",
-  destination: "49.258856, -122.895429",
+  origin: "49.155635, -123.125297",
+  origin_name: "Blundell and Garden City",
+  destination: "49.166760, -123.136480",
+  destination_name: "No 3 and Cook",
   arrival_time: "2000-01-01 02:30:00",
-  seats_available: 4,
   days: ["Mon","Tues","Wed"],
-  user_info: {name: "User 7 D Commute", email:"test@test.ca", phone: "604-802-1234"}
+  user_info: {name: "User 1 P Commute", email:"test@test.ca", phone: "604-802-1234"}
   )
+
+Place.create(
+  user_id:8, 
+  name: "Home",
+  cross_street:"24th and 168th", 
+  cross_street_point: "49.045860, -122.757011"
+  )
+
+Place.create(
+  user_id:8, 
+  name: "Work",
+  cross_street:"152nd and 60th", 
+  cross_street_point: "49.111953, -122.801065"
+  )
+
+Passengercommute.create(
+  user_id: 8,
+  origin: "49.045860, -122.757011",
+  origin_name: "24th and 168th",
+  destination: "49.111953, -122.801065",
+  destination_name: "152nd and 60th",
+  arrival_time: "2000-01-01 02:30:00",
+  days: ["Mon","Tues","Wed"],
+  user_info: {name: "User 1 P Commute", email:"test@test.ca", phone: "604-802-1234"}
+  )
+
 Drivercommute.create(
   user_id: 8,
-  car_type: "beetle",
-  origin_name: "Home - St Albans Rd and Jones Rd (Richmond)",
-  origin: "49.157374, -123.130940",
-  destination_name: "Work - Como Lake Ave and Crestwood Dr (Coquitlam)",
-  destination: "49.263468, -122.852723",
+  origin: "49.045860, -122.757011",
+  origin_name: "24th and 168th",
+  destination: "49.111953, -122.801065",
+  destination_name: "152nd and 60th",
   arrival_time: "2000-01-01 02:30:00",
-  seats_available: 5,
   days: ["Mon","Tues","Wed"],
-  user_info: {name: "User 8 D Commute", email:"test@test.ca", phone: "604-802-1234"}
+  user_info: {name: "User 1 P Commute", email:"test@test.ca", phone: "604-802-1234"}
   )
-Drivercommute.create(
+
+Place.create(
+  user_id:9, 
+  name: "Home",
+  cross_street:"Willingdon and Deer Lake", 
+  cross_street_point: "49.243125, -123.003309"
+  )
+
+Place.create(
+  user_id:9, 
+  name: "Work",
+  cross_street:"Victoria and 49th", 
+  cross_street_point: "49.225337, -123.065704"
+  )
+
+Passengercommute.create(
   user_id: 9,
-  car_type: "4 wheels",
-  origin_name: "Home - Cornish and 68th Ave (Vancouver)",
-  origin: "49.210531, -123.142071",
-  destination_name: "Work - E Mall and Thunderbird Blvd",
-  destination: "49.260103, -123.245706",
+  origin: "49.243125, -123.003309",
+  origin_name: "Willingdon and Deer Lake",
+  destination: "49.225337, -123.065704",
+  destination_name: "Victoria and 49th",
   arrival_time: "2000-01-01 02:30:00",
-  seats_available: 5,
   days: ["Mon","Tues","Wed"],
-  user_info: {name: "User 9 D Commute", email:"test@test.ca", phone: "604-802-1234"}
+  user_info: {name: "User 1 P Commute", email:"test@test.ca", phone: "604-802-1234"}
   )
-Drivercommute.create(
-  user_id: 10,
-  car_type: "school bus",
-  origin_name: "Home - Nelson Ave and Maitland St",
-  origin: "49.166522, -123.136471",
-  destination_name: "Work - Schoolhouse and Austin Ave",
-  destination: "49.283003, -123.115422",
-  arrival_time: "2000-01-01 02:30:00",
-  seats_available: 1,
-  days: ["Mon","Tues","Wed"],
-  user_info: {name: "User 10 D Commute", email:"test@test.ca", phone: "604-802-1234"}
-  )
+
+
+
+# I won't put in a drivercommute id in yet because that association
+# should get made when both parties agree
+
+
+
+
 
