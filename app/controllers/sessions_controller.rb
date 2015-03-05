1b4423503d27004c5dce4999f0869dc5e61ed0bd
Emily <emrcraig@gmail.com>
2015-03-04 20:05:48 -0800

    remove unused files

--- app/controllers/sessions_controller.rb
+++ app/controllers/sessions_controller.rb
@@ -12,15 +12,10 @@ class SessionsController < ApplicationController
     else
       render :new
     end
-    # user = User.first
-
-    # session[:user_id] = user.id
-    # redirect_to root_path
   end
 
   def destroy
     session[:user_id] = nil
-    # render nothing: true, status: 204
     redirect_to root_path
   end
 end
