e39958cca833f8ec1617f6e1bb3e154e09f4a04a
Gabriel Leung <gabriel.leung.607@gmail.com>
2015-03-04 20:14:58 -0800

    fixed twilio controller

--- app/controllers/twilio_controller.rb
+++ app/controllers/twilio_controller.rb
@@ -9,26 +9,26 @@ class TwilioController < ApplicationController
     auth_token = ENV['TWILIO_TOKEN']
     @client = Twilio::REST::Client.new account_sid, auth_token
     @from_number = ENV['TWILIO_NUMBER']
-    @recepient1_name = current_user.firstname
+    @recipient1_name = current_user.firstname
 
-    @recepient2_name = params["request_receiver"]["name"]
-    @recepient1_number = current_user.phone
-    @recepient2_number = params["request_receiver"]["phone"]
+    @recipient2_name = params["request_receiver_name"]
+    @recipient1_number = current_user.phone
+    @recipient2_number = params["request_receiver_phone"]
 
     if params["status"] == "accept"
-      @recepient1_number = "+17783231717"
-      @recepient2_number = "+17789776806"
-      recepient1_body = "Hi #{@recepient1_name}, your RideOrbit commute has been finalized! You'll be commuting with #{@recepient2_name}, who can be reached at #{@recepient2_number}. Happy networking!"
-      recepient2_body = "Hi #{@recepient2_name}, your RideOrbit commute has been finalized! You'll be commuting with #{@recepient1_name}, who can be reached at #{@recepient1_number}. Happy networking!"
+      @recipient1_number = "+17783231717"
+      @recipient2_number = "+17789776806"
+      recipient1_body = "Hi #{@recipient1_name}, your RideOrbit commute has been finalized! You'll be commuting with #{@recipient2_name}, who can be reached at #{@recipient2_number}. Happy networking!"
+      recipient2_body = "Hi #{@recipient2_name}, your RideOrbit commute has been finalized! You'll be commuting with #{@recipient1_name}, who can be reached at #{@recipient1_number}. Happy networking!"
 
-      send_text(@recepient1_number, recepient1_body)
-      send_text(@recepient2_number, recepient2_body) 
+      send_text(@recipient1_number, recipient1_body)
+      send_text(@recipient2_number, recipient2_body) 
 
     else
-      @recepient2_number = "+17789776806"
-      body = "Hi #{@recepient2_name}, someone has made a request on RideOrbit to connect with you! Go check it out to accept or decline." 
+      @recipient2_number = "+17789776806"
+      body = "Hi #{@recipient2_name}, someone has made a request on RideOrbit to connect with you! Go check it out to accept or decline." 
 
-      send_text(@recepient2_number, body)
+      send_text(@recipient2_number, body)
     end
 
     render json: {
